f70b886d7ec32b9014d843ab9f67810b0f8dbe83
tjo <tjo@af7818e5-df21-49d5-b9e6-ecf3e1c3d4f7>
2009-02-06 00:16:56 +0000

    Make json object of jenarix graphics primitives from sdf
    git-svn-id: https://svn2.hosted-projects.com/jjinux/jenarix@5 af7818e5-df21-49d5-b9e6-ecf3e1c3d4f7

---
+++ sandbox/tj/sdf2json.py
@@ -0,0 +1,28 @@
+import simplejson
+import sys
+
+line = sys.stdin.readline()
+title = line.strip()
+line = sys.stdin.readline()
+line = sys.stdin.readline()
+line = sys.stdin.readline()
+natoms,nbonds,rest = line.strip().split(None,2)
+
+radius = {"C":1.4, "N":1.3, "O":1.2, "H":1.0,
+          "F":1.0, "Cl":2.0, "P":2.0, "S":2.0}
+sphere = list()
+coords = list()
+for i in range(0,int(natoms)):
+  line = sys.stdin.readline()
+  x,y,z,symbol,rest = line.strip().split(None,4)
+  coords.append([float(x),float(y),float(z)])
+  sphere.append([coords[i],radius[symbol]])
+
+cylinder = list()
+for i in range(0,int(nbonds)):
+  line = sys.stdin.readline()
+  a,b,type,rest = line.strip().split(None,3)
+  cylinder.append([coords[int(a)-1], coords[int(b)-1], 0.25])
+
+primitives = [title,{"atoms":["sphere",sphere],"bonds":["cylinder",cylinder]}]
+print simplejson.dumps(primitives)
