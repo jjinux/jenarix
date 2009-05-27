2874552f5b0fc4b8a7a2bd4e7cb8e6d75d838cdb
warren <warren@af7818e5-df21-49d5-b9e6-ecf3e1c3d4f7>
2009-05-27 18:55:01 +0000

    git-svn-id: https://svn2.hosted-projects.com/jjinux/jenarix@169 af7818e5-df21-49d5-b9e6-ecf3e1c3d4f7

--- trunk/src/libjx/rules/arch/Linux-x86_64.mk
+++ trunk/src/libjx/rules/arch/Linux-x86_64.mk
@@ -1,6 +1,6 @@
 
 
-ARCH_CFLAGS = -Wall -ansi -pedantic -std=gnu99 -m64 -DJX_64_BIT
+ARCH_CFLAGS = -Wall -ansi -pedantic -std=gnu99 -g -O3 -m64 -DJX_64_BIT
 ARCH_CXXFLAGS = -ansi -m64 -DJX_64_BIT
 
 JX_SRC_QUASH = 
