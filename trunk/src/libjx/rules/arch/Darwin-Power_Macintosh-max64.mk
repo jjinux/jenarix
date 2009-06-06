8c695427b749c7cc98bf19b9da4f2b9f61e41a75
warren <warren@af7818e5-df21-49d5-b9e6-ecf3e1c3d4f7>
2009-06-06 23:30:44 +0000

    optimizer settings
    git-svn-id: https://svn2.hosted-projects.com/jjinux/jenarix@263 af7818e5-df21-49d5-b9e6-ecf3e1c3d4f7

--- trunk/src/libjx/rules/arch/Darwin-Power_Macintosh-max64.mk
+++ trunk/src/libjx/rules/arch/Darwin-Power_Macintosh-max64.mk
@@ -1,5 +1,7 @@
 
-ARCH_CFLAGS = -Wall -ansi -pedantic -std=gnu99 -O3 -fast -fomit-frame-pointer -ffast-math -finline-limit=10000 -DJX_64_BIT -m64 
+ARCH_CFLAGS = -Wall -ansi -pedantic -std=gnu99 -O3 -fomit-frame-pointer -ffast-math -finline-limit=10000 -DJX_64_BIT -m64 
+# NOTE -fast causes segfault with GCC 4.0.1
+
 
 ARCH_CXXFLAGS = -ansi 
 
--- trunk/src/libjx/rules/arch/Darwin-Power_Macintosh-run64.mk
+++ trunk/src/libjx/rules/arch/Darwin-Power_Macintosh-run64.mk
@@ -1,5 +1,5 @@
 
-ARCH_CFLAGS = -Wall -ansi -pedantic -std=gnu99 -g -O3 -fast -DJX_64_BIT -m64
+ARCH_CFLAGS = -Wall -ansi -pedantic -std=gnu99 -g -O3 -DJX_64_BIT -m64
 
 ARCH_CXXFLAGS = -ansi 
 
