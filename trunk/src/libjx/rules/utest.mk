cd1a00fdd4314e0cab93fa0b4b2ca9b17b19da8c
warren <warren@af7818e5-df21-49d5-b9e6-ecf3e1c3d4f7>
2009-06-07 21:59:19 +0000

    bugfixes
    git-svn-id: https://svn2.hosted-projects.com/jjinux/jenarix@271 af7818e5-df21-49d5-b9e6-ecf3e1c3d4f7

--- trunk/src/libjx/rules/utest.mk
+++ trunk/src/libjx/rules/utest.mk
@@ -1,10 +1,14 @@
 
 
+# genericize the output to enable  comparisons
+
+TXFM=sed 's/xrange/range/;s/0x[a-f0-9]+/0xX/'
+
 all: 
-	../jx -u unit01.jx > out/unit01.out || (cat out/unit01.out; exit -1;)
+	../jx -u unit01.jx | $(TXFM) > out/unit01.out || (cat out/unit01.out; exit 0;)
 	diff -q out/unit01.out out/ref || tkdiff out/unit01.out out/ref
 
-	../jxp -u unit02.jxp > out/unit02.out || (cat out/unit02.out; exit -1;)
+	../jxp -u unit02.jxp | $(TXFM) > out/unit02.out || (cat out/unit02.out; exit 0;)
 	diff -q out/unit02.out out/ref || tkdiff out/unit02.out out/ref
 
 
