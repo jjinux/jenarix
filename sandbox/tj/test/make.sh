2beba3c427ff052f5299d7f0cb461329a655ba12
warren <warren@af7818e5-df21-49d5-b9e6-ecf3e1c3d4f7>
2009-06-16 17:45:17 +0000

    added libs
    git-svn-id: https://svn2.hosted-projects.com/jjinux/jenarix@316 af7818e5-df21-49d5-b9e6-ecf3e1c3d4f7

--- sandbox/tj/test/make.sh
+++ sandbox/tj/test/make.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -v
 
-gcc -I../../../trunk/src/libjx/include main_jxl.c show_ob.c ../../../trunk/src/libjx/libjx.a -o ctest
+gcc -I../../../trunk/src/libjx/include main_jxl.c show_ob.c ../../../trunk/src/libjx/libjx.a -lpthread -lm -o ctest
 
-g++ -I../../../trunk/src/libjx/include main_jxl.cpp show_ob.cpp ../../../trunk/src/libjx/libjx.a -o cpptest
+g++ -I../../../trunk/src/libjx/include main_jxl.cpp show_ob.cpp ../../../trunk/src/libjx/libjx.a -lpthread -o cpptest
 
 echo -e "now try:\n\n./ctest < drugs.json\n\n   and\n\n./cpptest < drugs.json"
 
