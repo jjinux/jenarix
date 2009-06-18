aaaaf6fc6a3fd7c7ec8e2957d6c54dd173125f98
tjo <tjo@af7818e5-df21-49d5-b9e6-ecf3e1c3d4f7>
2009-06-18 17:18:43 +0000

    gui obj pointer not necessary
    git-svn-id: https://svn2.hosted-projects.com/jjinux/jenarix@323 af7818e5-df21-49d5-b9e6-ecf3e1c3d4f7

--- sandbox/tj/gui/gui.cpp
+++ sandbox/tj/gui/gui.cpp
@@ -7,8 +7,8 @@ int main(int argc, char *argv[])
 {
 
   int exit_status = EXIT_FAILURE;
-  GuiCreator *gui = new GuiCreator();
-  if (argc > 1) gui->setOutputType(argv[1]);
+  GuiCreator gui = GuiCreator();
+  if (argc > 1) gui.setOutputType(argv[1]);
   //if(jx_ok( jx_os_process_init(argc,argv) )) {
   if(jx_ok( jx_os_process_init(0,NULL) )) {
     exit_status = EXIT_SUCCESS;
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
     
     //if(jx_ok( jx_main_exec_in_node(argc,argv,node) )) {
     if(jx_ok( jx_main_exec_in_node(0,NULL,node) )) {
-      gui->gui_run_from_node(node);
+      gui.gui_run_from_node(node);
     }
     
     jx_ob_free(node);
