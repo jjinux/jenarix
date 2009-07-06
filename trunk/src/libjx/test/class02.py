1da66914a0ab4a2d26bbfe6956112d1d326d6b58
warren <warren@af7818e5-df21-49d5-b9e6-ecf3e1c3d4f7>
2009-07-06 23:05:42 +0000

    git-svn-id: https://svn2.hosted-projects.com/jjinux/jenarix@364 af7818e5-df21-49d5-b9e6-ecf3e1c3d4f7

--- trunk/src/libjx/test/class02.py
+++ trunk/src/libjx/test/class02.py
@@ -22,10 +22,13 @@ class SquareMatrix:
 
 m1 = new(SquareMatrix,3000)
 
-#  jxp:     8.15 sec, 
-#  python : 3.52 sec
-#  -> JXP 2.3x slower
+print m1.dim
 
+#  jxp:    8.15 sec (optimized 4-6-8 build, w/o heap tracker, on mac pro)
+#  python: 3.52 sec
+#  -> jxp is 2.3x slower
+
+# note: unoptimized jxp w/ heap tracker takes 21.7 seconds.
 
 
 
