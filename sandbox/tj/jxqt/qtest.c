4c08bd2e9d2274ca231a560e6048f720c0c3acbe
tjo <tjo@af7818e5-df21-49d5-b9e6-ecf3e1c3d4f7>
2009-08-13 18:38:58 +0000

    use new jx_main_expose_builtins_in_node(jx_ob node) then addQtBuilins before jx_main_exec_in_node.
    git-svn-id: https://svn2.hosted-projects.com/jjinux/jenarix@427 af7818e5-df21-49d5-b9e6-ecf3e1c3d4f7

--- sandbox/tj/jxqt/qtest.c
+++ sandbox/tj/jxqt/qtest.c
@@ -1,6 +1,6 @@
-#include "jx_main_tools.h"
-
 #include "qtBuiltins.h"
+#include "jx_public.h"
+#include "jx_main_tools.h"
 
 int main(int argc, char *argv[])
 {
@@ -11,11 +11,11 @@ int main(int argc, char *argv[])
     exit_status = EXIT_SUCCESS;
 
     jx_ob node = jx_hash_new();
+    jx_main_expose_builtins_in_node(node);
     addQtBuiltins(node);
-    jx_jxon_dump(stderr, "node", node);
     if(jx_ok( jx_main_exec_in_node(0,NULL,node) )) {
+      jx_ob_free(node);
     }
-    jx_ob_free(node);
     jx_os_process_complete();
   }
 
