b310a9324890b484d655fb0962f76aa80acc0224
tjo <tjo@af7818e5-df21-49d5-b9e6-ecf3e1c3d4f7>
2009-08-13 18:51:11 +0000

    change to cpp
    git-svn-id: https://svn2.hosted-projects.com/jjinux/jenarix@428 af7818e5-df21-49d5-b9e6-ecf3e1c3d4f7

--- sandbox/tj/jxqt/qtBuiltins.h
+++ sandbox/tj/jxqt/qtBuiltins.h
@@ -7,6 +7,8 @@ void addQtBuiltins(jx_ob node);
 void exposeQtBuiltins(jx_ob names);
 static jx_bool jx_declare(jx_bool ok, jx_ob names, jx_char * ident, jx_native_fn fn);
 
+extern "C" {
 jx_ob qtPrint(jx_env *E, jx_ob payload);
+}
 
 #endif
